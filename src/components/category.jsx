d8a3d63df7f4e1a66ea3ad2297ceafbc8a0a9bbe
fajarbagass <fajarbagassetiawan17@gmail.com>
2024-08-13 21:13:09 +0700

    change background category-item

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 
 function Category() {
   const [brands, setBrands] = useState([]);
+  const [selectedBrand, setSelectedBrand] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
 
   const getAllBrands = async () => {
     try {
@@ -19,9 +21,17 @@ function Category() {
       state: { brand: brand },
     });
   };
+
   useEffect(() => {
+    if (location.state) {
+      if (location.state.brand) {
+        setSelectedBrand(location.state.brand);
+      } else {
+        setSelectedBrand("");
+      }
+    }
     getAllBrands();
-  }, []);
+  }, [location]);
   return (
     <div className="container category-brand">
       {brands ? (
@@ -30,7 +40,11 @@ function Category() {
             return (
               <div
                 key={data.id}
-                className="category-item text-label"
+                className={
+                  selectedBrand === data.name
+                    ? "category-item-selected text-label"
+                    : "category-item text-label"
+                }
                 role="button"
                 onClick={() => handleSearch(data.name)}
               >
